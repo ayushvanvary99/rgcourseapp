aca723da1514ad8711f9edea213e60ed2804c1e8
ayushvanvary99 <63124623+ayushvanvary99@users.noreply.github.com>
2021-06-16 00:48:13 +0530

    formupdate

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <section className="py-3">
             <Heading title="Contact Us"/>
             <div className="col-10 col-sm-8 mx-auto">
-                <form action="https://formspree.io/ayush.vanvary10@gmail.com" method="POST">
+                <form action="https://formspree.io/f/xzbygadn" method="POST">
                     <div className="form-group">
                         <input type="text" name="name" id="name"
                         placeholder="Your Name" className="form-control"
